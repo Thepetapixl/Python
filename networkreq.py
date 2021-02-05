685386bb118fc1e6a05d3208a3bc9678c66e7c49
Thepetapixl <apurvvidhate40@gmail.com>
2021-02-05 15:35:24 +0530

    api stuff with python(network requests)

--- networkreq.py
+++ networkreq.py
@@ -1,11 +1,31 @@
-from urllib import request
+from urllib import request, parse
 import json
+import os
 
-url = 'https://dog.ceo/api/breeds/list/all'
+url = 'https://api.thecatapi.com/v1/images/search'
+path = '/Users/admin/VScode/Python/networkreq.py'
 
-with request.urlopen(url) as response:
-    content = response.read()
+params = {
+    'limit' : 5,
+    'page' : 1,
+    'order' : 'Desc'
+}
+
+querystring = parse.urlencode(params)
+
+with request.urlopen(url + '?' + querystring) as response:
+    content = response.read().decode('utf-8')
+
+print(content)
+
+with open("file.json", "w+") as file:
+    file.write(content)
+
+# In Json Format
+
+# with request.urlopen(url) as response:
+#     content = response.read()
     
-data = json.loads(content)
+# data = json.loads(content)
 
-print(data['status'])
+# print(data['status'])
